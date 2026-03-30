cafd0cb41a5c278b1a86952501580b0456306503
QuincyRefentse <quincyrefentse@gmail.com>
2026-03-30 11:51:48 +0000

    first commit 2

--- js/register-animals.js
+++ js/register-animals.js
@@ -1,3 +1,29 @@
+/*
+document.getElementById('animalForm').onsubmit = async function(e) {
+    e.preventDefault();
+    const id = document.getElementById('animalId').value;
+    const fmd_status = document.getElementById('fmdStatus').value;
+    const location = document.getElementById('animalLocation').value;
+    const imageBase64 = document.getElementById('qrImagePreview').src;
+
+    const res = await fetch('/.netlify/functions/save-animal', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ id, fmd_status, location, imageBase64 })
+    });
+
+    if (res.ok) {
+        alert('Animal registered!');
+        document.getElementById('animalForm').reset();
+        document.getElementById('animalForm').style.display = "none";
+        document.getElementById('qrImagePreview').src = "";
+    } else {
+        alert('Error saving animal!');
+    }
+};
+
+*/ 
+/*
 document.getElementById('animal-register-form').onsubmit = async (e) => {
   e.preventDefault();
   const id = document.getElementById('animal-id').value;
@@ -12,3 +38,5 @@ document.getElementById('animal-register-form').onsubmit = async (e) => {
   });
   if (res.ok) alert('Animal Registered!');
 };
+
+*/
